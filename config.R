f219645c834cc388774fa71e7783f4acf375c996
Deuana <deuana.telles@hotmail.com>
2018-08-27 16:40:19 -0300

    Arquivos de configuração

---
+++ config.R
@@ -0,0 +1,27 @@
+# MONGODB AUTH DATA
+
+mongoUser <- ""  
+mongoPasswd <- ""
+mongoHost <- "127.0.0.1"
+mongoPort <- "27017"
+
+# TWITTER AUTH DATA
+
+consumerKey <- ""
+consumerSecret <- ""
+accessToken <- ""
+accessTokenSecret <- ""
+
+
+# APP PERSISTENCE DATA
+
+dbName <- "testedb"
+dbCollection <- "tweets"
+
+# SET SEARCH PARAMETERS (referTo: https://github.com/twintproject/twint/wiki/Module)
+
+searchParameters.Username <- ""
+searchParameters.Output <- ""
+searchParameters.Stats <- TRUE
+searchParameters.Store_json <- TRUE
+searchParameters.Profile_full <- TRUE
