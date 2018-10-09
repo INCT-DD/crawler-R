d0f3942f14900b789cd28089bf5d5531ebe07aa0
Deuana <deuana.telles@hotmail.com>
2018-10-09 18:38:34 -0300

    Melhoria do script

--- get_followers.R
+++ get_followers.R
@@ -1,35 +1,35 @@
 library("rtweet")
 library("mongolite")
 
-#Inform which directory is the file.(source('~/git/crawler-R/infos.R'))
-source(config.R)
-source(infos.R)
+# Inform which directory is the file.(source('~/git/crawler-R/infos.R'))
+source('config.R')
+
+# Get followers
+args <- commandArgs(trailingOnly=TRUE)
+profile <- args[1]
+followers <- as.numeric(args[2])
 
 # APP PERSISTENCE DATA
 
-dbName <- "followerslistdb"
-dbCollection <- "followerslist"
+dbName <- paste0(args[3], "db")
+dbCollection <-args[3]
 
-#Get followers
-args <- commandArgs(trailingOnly=TRUE)
-profile <- args[1]
 
-f1 <- get_followers(profile, n = 75000)
-page <- next_cursor(f1)
-Sys.sleep(15 * 60) 
-f2 <- get_followers(profile, n = 75000, page = page)
-f <- do.call("rbind", list(f1, f2))
+f<-data.frame()
+for(i in 1:(ceiling(followers/75000))){ 
+    f1 <- get_followers(profile, n = 75000)
+    page <- next_cursor(f1)
+    Sys.sleep(15 * 60) 
+    f2 <- get_followers(profile, n = 75000, page = page)
+    f <- do.call("rbind", list(f1, f2))
+}
+#nrow(f)
+
 # DATABASE CONNECTION
 
-mongoUrl <- URLencode(paste0("mongodb://",
-                             mongoUser, ":", mongoPasswd,
-                             "@", mongoHost, ":", mongoPort)
-)
-
-
 mongoConnection <- mongo(db = dbName,
-                         collection = dbCollection,
-                         url = mongoUrl)
+                         collection = dbCollection)
+                         #url = mongoUrl)
 
 # DATA PERSISTENCE TO DATABASE
 
